df04722070f3da773d7228f7028dd6552199cc3e
JayK <1129594783@qq.com>
2018-05-01 13:16:18 +0800

    20180501

--- 模拟电商网站/readme.md
+++ 模拟电商网站/readme.md
@@ -1,3 +1,10 @@
+<!-- TOC -->
+
+- [1. 项目](#1-项目)
+
+<!-- /TOC -->
+
+# 1. 项目
 
     a标签阻止点击
     javascript:void(0)
@@ -11,3 +18,21 @@
     outline:none
 
     点击搜索按钮,显示/消失 搜索框
+
+
+    jQuery append prepend 方法
+        clone()方法
+    筛选:    last()
+            first()
+            eq()
+            
+    animate参数
+    animate(params,[speed],[easing],[fn])
+    用于创建自定义动画的函数.
+    params: 一组包含作为动画属性和终值的样式属性和及其值的集合
+    speed:三种预定速度之一的字符串
+    easing:要使用的擦除效果的名称
+    fn:在动画完成时执行的函数
+
+
+    函数节流问题！
--- 模拟电商网站/script/index.js
+++ 模拟电商网站/script/index.js
@@ -1,16 +1,83 @@
 // 点击右侧按钮 显示搜索框
-var count = 0;
-
-$(".search .icon-sousuo").click(function(){
-    count++;
-    if(count%2 != 0){
-        $(".search-box").animate({
-            "left":0,
-        },300);
-    }
-    if(count%2 == 0){
-        $(".search-box").animate({
-            "left":260,
-        },300);
-    }
+$(document).ready(function(){
+    var count = 0;
+    $(".search .icon-sousuo").click(function(){
+        count++;
+        if(count%2 != 0){
+            $(".search-box").animate({
+                "left":0,
+            },300);
+        }
+        if(count%2 == 0){
+            $(".search-box").animate({
+                "left":260,
+            },300);
+        }
+    })
 })
+    
+$(document).ready(function(){
+    // 轮播图复制两个节点
+    $("#actionContainer ul").append( $("#actionContainer ul .item1").clone());
+    $("#actionContainer ul").prepend( $("#actionContainer ul .item4").clone());
+    
+    var idx = 1;
+    // 每个图片的宽度
+    var x = $("#actionContainer .carousel li").innerWidth();    
+    var lock = true;
+
+    function imgMove(){
+        idx++;
+        $("#actionContainer .carousel").animate({left:idx*-x},300,function(){
+            if(idx >= 5 ){
+                idx = 1;
+                $("#actionContainer .carousel").css("left",-x);
+            }
+        });
+    }  
+    var t = setInterval(imgMove,3000);
+
+    // 鼠标经过轮播图显示图标,离开隐藏
+    $("#actionContainer").mouseover(function(){
+        $("#actionContainer .iconfont").show();
+    });
+    $("#actionContainer").mouseout(function(){
+        $("#actionContainer .iconfont").hide();
+    });
+
+    $("#actionContainer .iconfont").hover(
+        function(){
+            $(this).css({color:"#000"})
+        },
+        function(){
+            $(this).css({color:"#fff",opacity:0.5})
+        }
+    )
+
+    // 点击右按钮切换图片
+    $(".right-Btn").click(function(){
+        clearInterval(t);
+        idx++;
+        $("#actionContainer .carousel").animate({"left":-x*idx},400,function(){
+            if(idx>=5){
+                idx = 1;
+                $("#actionContainer .carousel").css({"left":-x});
+            }
+        })
+        t = setInterval(imgMove,3000);
+    })
+
+    // 点击左按钮切换图片
+    $(".left-Btn").click(function(){
+        clearInterval(t);
+        idx--;
+        $("#actionContainer .carousel").animate({"left":-x*idx},400,function(){
+            if(idx <= 0){
+                idx = 4
+                $("#actionContainer .carousel").css({"left":-idx*x});
+            }
+        })
+        t = setInterval(imgMove,3000);
+    })
+})
+    
