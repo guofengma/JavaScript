febc48c58f9415ff1284c3b4964ae7db61b4f1c9
JayK <1129594783@qq.com>
2018-06-11 11:11:51 +0800

    20180611

--- 重写手风琴效果-原生JS方法/main.js
+++ 重写手风琴效果-原生JS方法/main.js
@@ -12,22 +12,38 @@
 
     // flag = true 表示展开的,false表示未展开
     var flag = false;
-
-    for(let i = 0; i < oTitle.length; i++){
+    // 先保存length,for循环不用每次都计算length
+    var length = oTitle.length;
+    for(let i = 0; i < length; i++){
 
         oTitle[i].index = i;
+        // 判断当前是否已点击,
+        oTitle[i].isClick = false;
 
         oTitle[i].onclick = function(){
-
-            oHide[lastIndex].style.height = '0';
-            oI[lastIndex].classList.remove("rotate");
-            oTitle[lastIndex].classList.remove("active");
-            // 当前项目展开样式
-            oHide[this.index].style.height = '225px';
-            oI[this.index].classList.add("rotate");
-            oTitle[this.index].classList.add("active");
-            // 当前点击过的序号赋值给上依次
-            lastIndex = this.index;
+            // 如果当前已经点击过,则清除它的样式
+            if(oTitle[this.index].isClick ){
+                oHide[this.index].style.height = '0';
+                oI[this.index].classList.remove("rotate");
+                oTitle[this.index].classList.remove('active');
+                // 当前点击后 设置开关为 false
+                oTitle[this.index].isClick = false;
+            // 如果当前没有点击过
+            }else{
+                oHide[lastIndex].style.height = '0';
+                oI[lastIndex].classList.remove("rotate");
+                oTitle[lastIndex].classList.remove("active");
+                // 清除上一次样式的时候 开关也要关掉
+                oTitle[lastIndex].isClick = false;
+                // 当前项目展开样式
+                oHide[this.index].style.height = '225px';
+                oI[this.index].classList.add("rotate");
+                oTitle[this.index].classList.add("active");
+                // 当前点击过的序号赋值给上次
+                lastIndex = this.index;
+                // 并且点击当前
+                oTitle[this.index].isClick = true;
+            }
         }
     }
     // 鼠标经过h5背景颜色改变,鼠标点击时改变自身背景颜色
--- 重写手风琴效果-原生JS方法/readme.md
+++ 重写手风琴效果-原生JS方法/readme.md
@@ -2,5 +2,5 @@
     transition
     transform
     IIFE
-
+    transparent
     
